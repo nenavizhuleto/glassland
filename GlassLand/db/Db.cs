3deb5ba608c70983f4cb74476eebb4a92e8c1406
nenavizhuleto <nenavizhu.leto@gmail.com>
2022-12-14 01:09:15 -0800

    db

--- GlassLand/db/Db.cs
+++ GlassLand/db/Db.cs
@@ -4,15 +4,18 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Markup;
 
 namespace GlassLand.db
 {
     
     public class Db
     {
+        // "Data Source = (localdb)\\MSSQLLocalDB;Initial Catalog = C:\\USERS\\ADMINISTRATOR\\SOURCE\\REPOS\\GLASSLAND\\GLASSLAND\\GLASSLAND.MDF;Integrated Security = True\r\n"
 
-        static string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Administrator\\source\\repos\\GlassLand\\GlassLand\\glassland.mdf;Integrated Security=True;Connect Timeout=30";
-        
+
+
+        static string connectionString = "Data Source = (localdb)\\MSSQLLocalDB;Initial Catalog = [DatabaseName];Integrated Security = True\r\n";
         public Db() {
 
         }
